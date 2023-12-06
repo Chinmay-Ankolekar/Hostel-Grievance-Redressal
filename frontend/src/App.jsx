305ea9742ae254ab52128172c001d342c7a1f145
Chinmay-Ankolekar <chinmaysankolekar@gmail.com>
2023-12-06 19:31:51 +0530

    signup and login component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     {/* <Login/> */}
+     <Register/>
     </>
   )
 }

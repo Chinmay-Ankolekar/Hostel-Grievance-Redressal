dfb627f595bc28d50a03081f7182dd6a2ef27cbc
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2023-12-09 17:02:16 +0000

    register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -8,12 +8,15 @@ function Register() {
   const [ password, setPassword ] = useState("")
   const [ phone, setPhone ] = useState("")
   const [role, setRole] = useState(Roles.STUDENT);
+  const [block_id, setBlock_id] = useState("");
+  const [usn, setUsn] = useState("");
+  const [room, setRoom] = useState("");
 
   const onSubmit = async(e) => {
     e.preventDefault();
 
     try {
-        const body = {full_name:fullname, email, password,phone, type: role}
+        const body = {full_name:fullname, email, password,phone, type: role, block_id, usn, room}
         const response = await fetch("http://localhost:3000/register", {
             method: "POST",
             headers: {"content-type": "application/json "},
@@ -205,6 +208,120 @@ function Register() {
                       </div>
 
                       <div>
+                        <label
+                          for="full-name"
+                          class="block text-sm mb-2 dark:text-white"
+                        >
+                          Block-Id
+                        </label>
+                        <div class="relative">
+                          <input
+                            type="text"
+                            id="full-name"
+                            name="full-name"
+                            class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                            required
+                            aria-describedby="full-name-error"
+                            onChange={(e) => setBlock_id(e.target.value)}
+                          />
+                          <div class="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
+                            <svg
+                              class="h-5 w-5 text-red-500"
+                              width="16"
+                              height="16"
+                              fill="currentColor"
+                              viewBox="0 0 16 16"
+                              aria-hidden="true"
+                            >
+                              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                            </svg>
+                          </div>
+                        </div>
+                        <p
+                          class="hidden text-xs text-red-600 mt-2"
+                          id="full-name-error"
+                        >
+                          Please enter your Block-Id
+                        </p>
+                      </div>
+
+                      <div>
+                        <label
+                          for="full-name"
+                          class="block text-sm mb-2 dark:text-white"
+                        >
+                          USN
+                        </label>
+                        <div class="relative">
+                          <input
+                            type="text"
+                            id="full-name"
+                            name="full-name"
+                            class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                            required
+                            aria-describedby="full-name-error"
+                            onChange={(e) => setUsn(e.target.value)}
+                          />
+                          <div class="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
+                            <svg
+                              class="h-5 w-5 text-red-500"
+                              width="16"
+                              height="16"
+                              fill="currentColor"
+                              viewBox="0 0 16 16"
+                              aria-hidden="true"
+                            >
+                              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                            </svg>
+                          </div>
+                        </div>
+                        <p
+                          class="hidden text-xs text-red-600 mt-2"
+                          id="full-name-error"
+                        >
+                          Please enter your USN
+                        </p>
+                      </div>
+
+                      <div>
+                        <label
+                          for="full-name"
+                          class="block text-sm mb-2 dark:text-white"
+                        >
+                          Room No.
+                        </label>
+                        <div class="relative">
+                          <input
+                            type="text"
+                            id="full-name"
+                            name="full-name"
+                            class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                            required
+                            aria-describedby="full-name-error"
+                            onChange={(e) => setRoom(e.target.value)}
+                          />
+                          <div class="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
+                            <svg
+                              class="h-5 w-5 text-red-500"
+                              width="16"
+                              height="16"
+                              fill="currentColor"
+                              viewBox="0 0 16 16"
+                              aria-hidden="true"
+                            >
+                              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                            </svg>
+                          </div>
+                        </div>
+                        <p
+                          class="hidden text-xs text-red-600 mt-2"
+                          id="full-name-error"
+                        >
+                          Please enter your Room.no
+                        </p>
+                      </div>
+
+                      <div>
                         <p className="text-sm text-white">Role</p>
                         <div className="flex justify-between mt-1 px-1 gap-x-11 dark:bg-slate-900 rounded-md text-sm">
                           <button
@@ -239,6 +356,7 @@ function Register() {
                           </button>
                         </div>
                       </div>
+
 
                       <button
                         type="submit"

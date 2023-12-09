e180a6371a4b2e1fbb1e33af5aeac8957ba66293
Chinmay-Ankolekar <chinmaysankolekar@gmail.com>
2023-12-09 22:32:49 +0530

    complaint

--- frontend/src/pages/Complaint.jsx
+++ frontend/src/pages/Complaint.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ComplaintForm = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+
+    if (!name || name.trim() === '') {
+        alert('Please enter a valid name.');
+        return; 
+      }
+    if (!description || description.trim() === '') {
+        alert('Please enter a valid complaint.');
+        return; 
+      }
+
+    try {
+        
+      const body = { description };
+      const response = await fetch("http://localhost:3000/complaints", {
+        method: "POST",
+        headers: { "content-Type": "application/json" , "Authorization" :"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfaWQiOjE3LCJ0eXBlIjoic3R1ZGVudCJ9LCJpYXQiOjE3MDIxMzcxMDIsImV4cCI6MTcwMjE0MDcwMn0.wrL4mFEMzAzqC_eiu1LKAQqSYWoivdlKeemaKsCJXv0"},
+        body: JSON.stringify(body),
+      });
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
 
-    
+
 
     <div className="max-w-lg bg-white shadow-md p-4 rounded-md mb-4 ml-11">
       <h2 className="text-xl font-semibold mb-4">Create Complaint</h2>
-      <form>
+      <form onSubmit={onSubmitForm}>
         <div className="mb-4">
           <label htmlFor="complaintName" className="block text-gray-700 text-sm font-bold mb-2">
             Complaint Name
@@ -16,18 +45,9 @@ const ComplaintForm = () => {
             type="text"
             id="complaintName"
             className="w-full border rounded-md py-2 px-3"
+            value={name}
+            onChange={(e)=> setName(e.target.value)}
             placeholder="Enter complaint name..."
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="complaintName" className="block text-gray-700 text-sm font-bold mb-2">
-            Room No.
-          </label>
-          <input
-            type="text"
-            id="complaintName"
-            className="w-full border rounded-md py-2 px-3"
-            placeholder="Enter Room No..."
           />
         </div>
         <div className="mb-4">
@@ -37,6 +57,8 @@ const ComplaintForm = () => {
           <textarea
             id="complaintDescription"
             className="w-full border rounded-md py-2 px-3"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
             placeholder="Enter complaint description..."
           />
         </div>
@@ -54,21 +76,42 @@ const ComplaintForm = () => {
 };
 
 const ComplaintsPage = () => {
+  const [ complaints , setComplaints ] = useState([]);
+
+  const getComplaints = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/complaints");
+      const jsonData = await response.json();
+
+      setComplaints(jsonData);
+      
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getComplaints();
+  }, []);
+
+  console.log(complaints);
+
   return (
     <div className="flex flex-col items-start justify-start min-h-screen gap-x-10">
 
       {/* Heading */}
       <h1 className="mt-5 ml-5 mb-5 text-3xl">Complaints</h1>
 
-      <div className="flex">
+    { complaints.map((complaint) => (
+      <div key={complaint.complaint_id} className="flex">
         {/* Complaint Card */}
         <div className="max-w-xl bg-white shadow-md p-4 rounded-md mb-[160px] ml-8">
-          <h2 className="text-lg font-semibold mb-2">Complaint Title</h2>
+          <h2 className="text-lg font-semibold mb-2">{complaint.name}</h2>
           <div className="flex items-center mb-2">
             {/* Left Column: Description */}
             <div className="flex-1 pr-4">
               <p className="text-gray-700">
-                This is the description of the card. You can add more details here as needed.
+                {complaint.description}
               </p>
             </div>
 
@@ -84,6 +127,7 @@ const ComplaintsPage = () => {
         {/* Complaint Form */}
         <ComplaintForm />
       </div>
+ ))}
     </div>
   );
 };

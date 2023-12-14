63d0c9fe23782a2fd6378afbeab88335cbf99909
Chinmay-Ankolekar <chinmaysankolekar@gmail.com>
2023-12-14 13:07:15 +0530

    ui design updated

--- frontend/src/pages/Complaint.jsx
+++ frontend/src/pages/Complaint.jsx
@@ -63,9 +63,11 @@ const ComplaintForm = () => {
   };
 
   return (
-   
 
-<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+    
+   <>
+
+{/* <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
   <div class="mx-auto max-w-2xl">
     <div class="text-center">
       <h2 class="text-xl text-gray-800 font-bold sm:text-3xl">
@@ -99,9 +101,68 @@ const ComplaintForm = () => {
     </div>
   
   </div>
-</div>
+</div> */}
 
 
+<section class="bg-gray-100 py-12 text-gray-800 sm:py-24 h-full">
+  <div class="bg-gray-100 mx-auto flex max-w-md flex-col rounded-lg lg:max-w-screen-xl lg:flex-row">
+    <div class="max-w-2xl px-4 lg:pr-24">
+      <p class="mb-2 text-blue-600">Hostel Grievance Redressal</p>
+      <h3 class="mb-5 text-3xl font-semibold">Submit Your Grievance</h3>
+      <p class="mb-16 text-lg text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur, corrupti asperiores voluptatum labore eligendi quisquam. Id quae, laboriosam saepe facere ea asperiores!</p>
+      <div class="mb-5 flex font-medium">
+        <div class="mr-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-7 w-7 text-blue-500">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m-6 3.75l3 3m0 0l3-3m-3 3V1.5m6 9h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75" />
+          </svg>
+        </div>
+        <div class="">
+          <p class="mb-2">Swift Grievance Resolution</p>
+          <span class="font-normal text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum porro molestias quaerat maxime modi.</span>
+        </div>
+      </div>
+      <div class="mb-5 flex font-medium">
+        <div class="mr-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-7 w-7 text-blue-500">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+          </svg>
+        </div>
+        <div class="">
+          <p class="mb-2">Confidentiality Assured</p>
+          <span class="font-normal text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum porro molestias quaerat maxime modi.</span>
+        </div>
+      </div>
+      <div class="mb-5 flex font-medium">
+        <div class="mr-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-7 w-7 text-blue-500">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+          </svg>
+        </div>
+        <div class="">
+          <p class="mb-2">Easy Communication</p>
+          <span class="font-normal text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum porro molestias quaerat maxime modi.</span>
+        </div>
+      </div>
+    </div>
+    <div class="border border-gray-100 shadow-gray-500/20 mt-8 mb-8 max-w-md bg-white shadow-sm sm:rounded-lg sm:shadow-lg lg:mt-0">
+      <div class="relative border-b border-gray-300 p-4 py-8 sm:px-8">
+        <h3 class="mb-1 inline-block text-3xl font-medium"><span class="mr-4">Submit Complaint</span><span class="inline-block rounded-md bg-blue-100 px-2 py-1 text-sm text-blue-700 sm:inline">Quick Response</span></h3>
+        <p class="text-gray-600">Contact us for hostel grievance redressal</p>
+      </div>
+      <div class="p-4 sm:p-8">
+        <input id="name" type="text" class="mt-1 w-full resize-y overflow-auto rounded-lg border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none hover:border-blue-500" placeholder="Enter your name" />
+        <input id="email" type="email" class="peer mt-8 w-full resize-y overflow-auto rounded-lg border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none hover:border-blue-500" placeholder="Enter your email" />
+        <div class="peer-invalid:block mt-1 hidden text-left text-xs text-rose-600">Email format Incorrect. Please complete the email</div>
+        <label class="mt-5 mb-2 inline-block max-w-full">Tell us about your grievance</label>
+        <textarea id="about" class="mb-8 w-full resize-y overflow-auto rounded-lg border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none hover:border-blue-500"></textarea>
+        <button class="w-full rounded-lg border border-blue-700 bg-blue-700 p-3 text-center font-medium text-white outline-none transition focus:ring hover:border-blue-700 hover:bg-blue-600 hover:text-white">Submit</button>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+</>
   );
 };
 
@@ -146,7 +207,47 @@ const ComplaintsPage = () => {
   return (
     
     <>
-    <div >
+   <div className="bg-gray-100 p-4 sm:p-8 md:p-10 h-screen">
+  <h1 className="text-2xl font-bold mb-8">Complaints</h1>
+  {complaints.length === 0 ? (
+    <p className="ml-4 mt-2 text-gray-600 text-xl">
+      No complaints registered yet.
+    </p>
+  ) : (
+    <div className="container mx-auto grid gap-8 md:grid-cols-3 sm:grid-cols-1">
+      {complaints.map((complaint) => (
+        <div key={complaint.complaint_id} className="relative flex h-full flex-col rounded-md border border-gray-200 bg-white p-2.5 hover:border-gray-400 sm:rounded-lg sm:p-5">
+          <div className="text-lg mb-2 font-semibold text-gray-900 hover:text-black sm:mb-1.5 sm:text-2xl">
+            {complaint.name}
+          </div>
+          <p className="text-sm">Created on {formatTimestamp1(complaint.created_at)}</p>
+          <p className="mb-4 text-sm">
+            {complaint.assigned_at ? `Completed on ${formatTimestamp(complaint.assigned_at)}` : null}
+          </p>
+          <div className="text-md leading-normal text-gray-400 sm:block">
+            {complaint.description}
+          </div>
+          <button
+            className={clsx(
+              "group flex w-1/3 mt-3 cursor-pointer items-center justify-center rounded-md px-4 py-2 text-white transition text-sm",
+              complaint.is_completed ? "bg-green-500" : "bg-red-600"
+            )}
+            onClick={() => handleApproval(complaint.id)}
+          >
+            <span className="group flex w-full items-center justify-center rounded py-1 text-center font-bold">
+              {complaint.is_completed ? "Completed" : "Not Completed"}
+            </span>
+          </button>
+        </div>
+      ))}
+    </div>
+  )}
+
+  <ComplaintForm />
+</div>
+
+
+    {/* <div >
    
 <h1 className="mt-4 ml-4 mb-1 text-2xl font-semibold">Complaints</h1>
 {complaints.length === 0 ? (
@@ -190,7 +291,7 @@ const ComplaintsPage = () => {
       )}
          <ComplaintForm />
    
-    </div>
+    </div> */}
    
 </>
   );
--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
     return( 
         <>
       
- <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0">
+ {/* <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0">
       <nav className="relative max-w-7xl w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8" aria-label="Global">
         <div className="flex items-center justify-between">
           <a className="flex-none text-2xl font-semibold "  href="/" aria-label="Brand">
@@ -57,7 +57,37 @@ function Navbar() {
           </div>
         </div>
       </nav>
-    </header>
+    </header> */}
+
+<header class="shadow bg-white">
+  <div class="relative flex max-w-screen-xl flex-col overflow-hidden px-4 py-4 md:mx-auto md:flex-row md:items-center">
+    <a href="#" class="flex items-center whitespace-nowrap text-2xl font-black">
+      <span class="mr-2 text-4xl text-blue-600">
+        {/* <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M6.925 16.875Q5.2 16.225 4.1 14.713Q3 13.2 3 11.25q0-1.975.938-3.513Q4.875 6.2 6 5.15q1.125-1.05 2.062-1.6L9 3v2.475q0 .625.45 1.062q.45.438 1.075.438q.35 0 .65-.15q.3-.15.5-.425L12 6q.95.55 1.625 1.35t1.025 1.8l-1.675 1.675q-.05-.6-.287-1.175q-.238-.575-.638-1.05q-.35.2-.738.287q-.387.088-.787.088q-1.1 0-1.987-.612Q7.65 7.75 7.25 6.725q-.95.925-1.6 2.062Q5 9.925 5 11.25q0 .775.275 1.462q.275.688.75 1.213q.05-.5.287-.938q.238-.437.588-.787L9 10.1l2.15 2.1q.05.05.1.125t.1.125l-1.425 1.425q-.05-.075-.087-.125q-.038-.05-.088-.1L9 12.925l-.7.7q-.125.125-.212.287q-.088.163-.088.363q0 .3.175.537q.175.238.45.363ZM9 10.1Zm0 0ZM7.4 22L6 20.6L19.6 7L21 8.4L17.4 12H21v2h-5.6l-.5.5l1.5 1.5H21v2h-2.6l2.1 2.1l-1.4 1.4l-2.1-2.1V22h-2v-4.6l-1.5-1.5l-.5.5V22h-2v-3.6Z" /></svg> */}
+      </span>
+      <span class="text-black">SJCE Boy's Hostel</span>
+    </a>
+    <input type="checkbox" class="peer hidden" id="navbar-open" />
+    <label class="absolute top-5 right-7 cursor-pointer md:hidden" for="navbar-open">
+      <span class="sr-only">Toggle Navigation</span>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+      </svg>
+    </label>
+    <nav aria-label="Header Navigation" class="peer-checked:mt-8 peer-checked:max-h-56 flex max-h-0 w-full flex-col items-center justify-between overflow-hidden transition-all md:ml-24 md:max-h-full md:flex-row md:items-start">
+      <ul class="flex flex-col items-center space-y-2 md:ml-auto md:flex-row md:space-y-0">
+
+        <li class="text-gray-600 md:mr-12 hover:text-blue-600"><a href="/">Dashboard</a></li>
+        <li class="text-gray-600 md:mr-12 hover:text-blue-600">
+          <button class="rounded-md border-2 border-blue-600 px-6 py-1 font-medium text-blue-600 transition-colors hover:bg-blue-600 hover:text-white" onClick={logout}>Logout</button>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+
+
 
 
 

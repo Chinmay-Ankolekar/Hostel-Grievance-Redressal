2cfacc47699a49db70bb086ec3f3249f41eeb363
Chinmay-Ankolekar <chinmaysankolekar@gmail.com>
2023-12-15 08:33:50 +0530

    wardencomplaint updated

--- frontend/src/pages/WardenComplaint.jsx
+++ frontend/src/pages/WardenComplaint.jsx
@@ -67,12 +67,13 @@ const WardenComplaints = () => {
         method: 'DELETE',
         headers: GetAuthHeader(),
       });
-
+    
       if (response.ok) {
         setComplaints(complaints.filter(complaint => complaint.id !== id));
       } else {
         console.error('Failed to delete complaint');
       }
+      
     } catch (error) {
       console.error('Error deleting complaint:', error);
     }
@@ -136,7 +137,7 @@ const WardenComplaints = () => {
       {complaints.map((complaint) => (
         <div key={complaint.complaint_id} className="relative flex h-full flex-col rounded-md border border-gray-200 bg-white p-2.5 hover:border-gray-400 sm:rounded-lg sm:p-5">
           <div className="text-lg mb-2 font-semibold text-gray-900 hover:text-black sm:mb-1.5 sm:text-2xl">
-            {complaint.name}
+            {complaint.name} (Room No.{complaint.room})
           </div>
           <p className="text-sm">Created on {formatTimestamp1(complaint.created_at)}</p>
           <p className="mb-4 text-sm">
@@ -161,9 +162,9 @@ const WardenComplaints = () => {
   <button
       className="group flex w-1/3 mt-3 ml-3 cursor-pointer items-center justify-center rounded-md px-4 py-2 text-white transition text-sm bg-red-600" onClick={()=>deleteComplaint(complaint.id)}
     >
-      <span className="group flex w-full items-center justify-center rounded py-1 text-center font-bold">
+      <a className="group flex w-full items-center justify-center rounded py-1 text-center font-bold" href="/">
         Delete
-      </span>
+      </a>
     </button>
 </div>
 
